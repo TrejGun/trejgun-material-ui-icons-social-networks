ef9255fd995b866109380ad4f80b1514615ae9e5
TrejGun <trejgun@gmail.com>
2019-09-25 15:52:02 +0300

    initial

---
+++ src/flickr.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {SvgIcon} from "@material-ui/core";
+import {SvgIconProps} from "@material-ui/core/SvgIcon/SvgIcon";
+
+
+export const Flickr: React.FC<SvgIconProps> = props => {
+  return (
+    <SvgIcon {...props}>
+      <rect fill="#E12C92" x="0" y="0" width="23.9858421" height="23.9858421" />
+      <circle fill="#FFFFFF" cx="7.16852632" cy="11.7232105" r="4.23263158" />
+      <circle fill="#FFFFFF" cx="16.8173158" cy="12.2626316" r="4.23263158" />
+    </SvgIcon>
+  );
+};
